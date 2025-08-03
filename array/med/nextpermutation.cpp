3c5a62ab1e9debc79b13c3a3fa6651fa62b6dbea
Balodi <balodiaryan5@gmail.com>
2025-08-04 00:19:47 +0530

    Array med 08 next permutation

--- array/med/nextpermutation.cpp
+++ array/med/nextpermutation.cpp
@@ -1,60 +1,28 @@
 #include<iostream>
 #include<vector>
+#include<stdlib.h>
 using namespace std;
 
-// Brute approach -> consider all sub-arrays and do sum
-
-int brute(vector<int> &arr, int n) {
-    int maxi = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            int sum = 0;
-            for (int k = i; k <= j; k++) {
-                sum = sum + arr[k];
-                maxi = max(sum,maxi);
-            }
+vector<int> Next_Permutation(vector<int> &arr, int n) {
+    int index = -1;
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] < arr[i + 1]) {
+            swap(arr[i], arr[i + 1]);
+            break;
         }
     }
-    return maxi;
-}
+    if (index == -1) return arr;
 
-// Better approach -> do a running sum, no need for the third loop; just store the sum till now in sum and add the current element to it.
-
-int better(vector<int> &arr, int n) {
-    int maxi = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        int sum = 0;
-        for (int j = i; j < n; j++) {
-            sum = sum + arr[j];
-            maxi = max(maxi,sum);
+    for (int i = n - 1; i > index; i--) {
+        if (arr[i] > arr[index]) {
+            swap(arr[i], arr[index]);
+            break;
         }
     }
-    return maxi;
 
-}
+    reverse(arr.begin() + index + 1, arr.end());
 
-// Optimal approach -> kadane's algo
-// iterate in the array and reset the sum if sum is -ve as a -ve number cannot contribute to "MAXIMUM SUM".
-
-vector<int> optimal(vector<int> &arr, int n) {
-    int maxi = INT_MIN;
-    int sum = 0;
-    int ansStart = -1; int ansEnd = -1; int start = -1;
-    for (int i = 0; i < n; i++) {
-        if (sum == 0) start = i;
-        sum = sum + arr[i];
-
-        if (sum > maxi) {
-            maxi = sum;
-            ansStart = start;
-            ansEnd = i;
-        }
-
-        if (sum < 0) {
-            sum = 0;
-        }
-    }
-    return {maxi,ansStart, ansEnd};
+    return arr;
 }
 
 int main() {
@@ -68,35 +36,13 @@ int main() {
         cin >> arr[i];
     }
 
-    string choice;
-    cout << "Enter 'brute' for brute approach, 'better' for better approaach or 'optimal' for optimal approach: ";
-    cin >> choice;
+    vector<int> answer = Next_Permutation(arr,n);
 
-    int answer;
-
-    if(choice == "brute") {
-        answer = brute(arr,n);
-        cout << "The maximum subarray sum is: " << answer << endl;
+    cout << "The next permutation of the input array is as follows: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    else if (choice == "better") {
-        answer = better(arr,n);
-        cout << "TThe maximum subarray sum is:: " << answer << endl;
-    }
-    else if (choice == "optimal") {
-        vector<int> answer_optimal = optimal(arr,n);
-        int maxSum = answer_optimal[0];
-        int start = answer_optimal[1];
-        int end = answer_optimal[2];
-        cout << "The maximum subarray sum is: " << maxSum << endl;
-        cout << "The elements of the sub-array are: [ ";
-        for (int i = start; i <= end; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "]" << endl;
-    }
-    else {
-        cout << "Invalid choice; please choose between 'brute', 'better' and 'optimal' ONLY.";
-    }
+    cout << endl;
 
     return 0;
 

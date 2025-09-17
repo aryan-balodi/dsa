ca86ebae9a648669f9c990a05850916ca20cc692
balodi <balodiaryan5@gmail.com>
2025-09-17 23:32:12 +0530

    DLL creation

---
+++ linked_list/Double_LL/creation.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class DListNode {
+    public:
+    int data;
+    DListNode *next;
+    DListNode *prev;
+
+    public:
+    DListNode(int data1, DListNode *next1, DListNode *prev1) {
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+
+    DListNode(int data1) {
+        data = data1;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+DListNode *Arr2DLL(vector<int> &arr) {
+    DListNode *head = new DListNode(arr[0]);
+    DListNode *prev = head;
+    
+    for (int i = 1; i < arr.size(); i++) {
+        DListNode *temp = new DListNode(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(DListNode *head) {
+    DListNode *temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "END" << endl;
+}
+
+int main() {
+    vector<int> arr = {2,4,6,8,10};
+
+    DListNode *head = Arr2DLL(arr);
+
+    print(head);
+
+}

0e215430b484e4606709bb52464e87a8862cf98a
balodi <balodiaryan5@gmail.com>
2025-10-27 23:51:56 +0530

    merge K lists -pq

---
+++ linked_list/Single_LL/mergeKlists.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class ListNode {
+    public:
+    int data;
+    ListNode *next;
+
+    public:
+    ListNode(int data1, ListNode *next1) {
+        
+    }
+};
+
+int main() {
+
+}
